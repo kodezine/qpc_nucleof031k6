6ac3f527d143fe1f704291898bcc520e78061793
sohal <tosohal@gmail.com>
2023-09-18 17:07:27 +0200

    new unit tests

--- src/test/target/ut_source.c
+++ src/test/target/ut_source.c
@@ -2,11 +2,15 @@
 #include "cmock.h"
 #include "source.h"
 
-void test_addOneToByte(void)
+void test_addOneToByte_AllOK(void)
 {
-    volatile static uint8_t retVal = 0;
+    volatile uint8_t retVal = 0;
     retVal = addOneToByte((const uint8_t)2);
-    TEST_ASSERT_EQUAL_HEX8(3, retVal);
-    retVal = addOneToByte((const uint8_t)255);
-    TEST_ASSERT_EQUAL_HEX8(0, retVal);
+    TEST_ASSERT_EQUAL_HEX8_MESSAGE(3, retVal, "The output is bad");
+}
+
+void test_addOneToByte_Overflow(void)
+{
+    volatile uint8_t retVal = addOneToByte((const uint8_t)255);
+    TEST_ASSERT_EQUAL_HEX8_MESSAGE(3, retVal, "The output is not overflowing");
 }
