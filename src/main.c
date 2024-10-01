8918a4e8091ec96c20bf2de8f66fbab8e3cbd3a1
Sohal <tosohal@gmail.com>
2024-10-01 03:56:44 +0200

    Kind of works

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdbool.h>
 #include "main.h"
-
+#include "SEGGER_RTT.h"
 uint8_t addOneToByte(const uint8_t byte)
 {
     return(byte + 1);
+}
+
+void delayMe(void)
+{
+    for (int i = 0; i < 5000; i++)
+    {
+        asm("nop");
+    }
 }
 
 void SystemInit(void)
@@ -12,9 +20,17 @@ void SystemInit(void)
 }
 int main (void)
 {
+    SEGGER_RTT_Init();
+    
+  SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+
+  SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n\r\n");
     SystemInit();
     do{
         addOneToByte(1);
+          SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n\r\n");
+          delayMe();
+
     }while(true);
     return 0;
 }
